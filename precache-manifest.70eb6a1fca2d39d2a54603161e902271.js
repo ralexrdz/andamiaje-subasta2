e62691e13d0eafbb86fc79e84fa2e02bec8abd9f
ralex <ralex.rdz@gmail.com>
2019-01-11 02:27:16 -0600

    Updates

---
+++ precache-manifest.70eb6a1fca2d39d2a54603161e902271.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26da766842b2940deb88",
+    "url": "/andamiaje-subasta2/static/css/main.52b3d219.chunk.css"
+  },
+  {
+    "revision": "26da766842b2940deb88",
+    "url": "/andamiaje-subasta2/static/js/main.26da7668.chunk.js"
+  },
+  {
+    "revision": "2cc322da77a692d4676f",
+    "url": "/andamiaje-subasta2/static/js/1.2cc322da.chunk.js"
+  },
+  {
+    "revision": "d4bd7eac935cc20741dc",
+    "url": "/andamiaje-subasta2/static/js/runtime~main.d4bd7eac.js"
+  },
+  {
+    "revision": "90f61a90fc672bc32e147fb4664fe263",
+    "url": "/andamiaje-subasta2/index.html"
+  }
+];
